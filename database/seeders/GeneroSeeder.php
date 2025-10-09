629ad92142dbdca6586cb3d7cf39d1ed71d62c61
ybielz <119979077+ybielz@users.noreply.github.com>
2025-10-09 03:36:02 -0300

    Update GeneroSeeder.php

--- database/seeders/GeneroSeeder.php
+++ database/seeders/GeneroSeeder.php
@@ -18,18 +18,7 @@ class GeneroSeeder extends Seeder
             ['id'=> 2,'nome'=>"Romance"],//2
             ['id'=> 3,'nome'=>"Comédia"],//3
             ['id'=> 4,'nome'=>"Drama"],//4
-            ['id'=> 5,'nome'=>"Ação"],//5
-            ['id'=> 6,'nome'=>"Sci-fi"],//6
-            ['id'=> 7,'nome'=>"Animado"],//7
-            ['id'=> 8,'nome'=>"Pós-Apocalíptico"],//8
-            ['id'=> 9,'nome'=>"Crime"],//9
-            ['id'=> 10,'nome'=>"Aventura"],//10
-            ['id'=> 11,'nome'=>"Mistério"],//11
-            ['id'=> 12,'nome'=>"Super-Herói"],//12
-            ['id'=> 13,'nome'=>"Noir"],//13
-            ['id'=> 14,'nome'=>"Musical"],//14
-            ['id'=> 15,'nome'=>"Histórico"],//15
-
+            ['id'=> 5,'nome'=>"Ação"],//5  
         ]);
     }
 }
